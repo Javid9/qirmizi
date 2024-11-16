5ec96a15aa4b35ce79dd099b753a8eb0b8b9aafa
harunozbilgi <ozbilgiharun04@gmail.com>
2024-11-16 15:33:56 +0400

    request.js filter islemi duzeltili

--- WebApp/Controllers/SpeakerController.cs
+++ WebApp/Controllers/SpeakerController.cs
@@ -1,84 +1,33 @@
 ﻿using Application.Services;
-using Domain.Dtos.Speaker;
-using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApp.Controllers
+namespace WebApp.Controllers;
+
+public class SpeakerController(ISpeakerService speakerService) : Controller
 {
-    public class SpeakerController(ISpeakerService speakerService) : Controller
+    [Route("/{lang}/spikerlər")]
+    public async Task<IActionResult> Index(string lang = "az")
     {
-        [Route("/{lang}/spikerlər")]
-        public async Task<IActionResult> Index(string lang = "az")
-        {
-            ViewBag.Lang = HttpContext.Session.GetString("lang") ?? lang;
+        ViewBag.Lang = HttpContext.Session.GetString("lang") ?? lang;
 
+        var speakers = await speakerService.GetClientAllAsync(lang);
+
+        return View(speakers.Data);
+    }
+
+
+    [HttpGet]
+    [Route("/{lang}/spikerler/ara")]
+    public async Task<IActionResult> Search(string query, string lang)
+    {
+        if (string.IsNullOrEmpty(query))
+        {
             var speakers = await speakerService.GetClientAllAsync(lang);
-
-            return View(speakers.Data);
+            return PartialView(speakers.Data);
         }
 
+        var response = await speakerService.SearchSpeakersAsync(query, lang);
 
-        //[HttpGet]
-        //[Route("/{lang}/spikerler/ara")]
-        //public async Task<IActionResult> Search(string query, string lang = "az")
-        //{
-        //    ViewBag.Lang = HttpContext.Session.GetString("lang") ?? lang;
-
-
-        //    if (string.IsNullOrEmpty(query))
-        //    {
-        //        return View("Index", new List<SpeakerClientDto>());
-        //    }
-
-        //    var response = await speakerService.SearchSpeakersAsync(query, lang);
-
-        //    return View("Index", response.Data);
-        //}
-
-
-
-        [HttpGet]
-        [Route("/{lang}/spikerler/ara")]
-        public async Task<IActionResult> Search(string query, string lang = "az")
-        {
-             //if (string.IsNullOrEmpty(query))
-             //   {
-             //       return BadRequest("Search query is required");
-             //   }
-
-             //   var response = await speakerService.SearchSpeakersAsync(query, lang);
-
-             //   if (response == null)
-             //   {
-             //       return NotFound("No speakers found");
-             //   }
-
-             //   return Ok(response.Data);
-
-            return Ok(new
-            {
-                data = new List<SpeakerClientDto>
-                {
-                    new SpeakerClientDto
-                    {
-                        Id = "1",
-                        FileCode = "1",
-                        Instagram = "instagram",
-                        Facebook = "facebook",
-                        Twitter = "twitter",
-                        LinkedIn = "linkedin",
-                        FullName = "Full Name",
-                        Postion = "Position",
-                        Text = "Text",
-                        Title = "Title"
-                    }
-                }
-            });
-            
-
-        }
-
-
-
+        return response.IsSuccess ? PartialView(response.Data) : PartialView();
     }
 }
